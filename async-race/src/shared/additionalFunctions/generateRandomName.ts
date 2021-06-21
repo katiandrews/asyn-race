21ca41158e38cf9b8f6e252cd8b61b0e54e7e157
katiandrews <andryushina.eka@gmail.com>
2021-06-21 11:05:24 +0200

    refactor: refactor random car generation funcrtion

--- async-race/src/shared/additionalFunctions/generateRandomName.ts
+++ async-race/src/shared/additionalFunctions/generateRandomName.ts
@@ -1,12 +1,11 @@
 export function generateRandomName(): string {
-  const names = ['Tesla', 'Volkswagen', 'BMW', 'Lexus', 'Mitsubishi', 'Toyota',
+  const brands = ['Tesla', 'Volkswagen', 'BMW', 'Lexus', 'Mitsubishi', 'Toyota',
     'Mazda', 'Audi', 'Opel', 'Skoda', 'KIA', 'Hyndai', 'Nissan',
     'Ford', 'Jaguar', 'Mercedes', 'Volvo', 'Honda', 'Subaru'];
   const models = ['Tiguan', 'Micra', 'Avensis', 'Solaris', 'Elantra', 'X3', 'X5', 'Galant',
     'Lancer', 'Mondeo', 'Octavia', 'Skyline', 'Juke', 'Sportage', 'Mustang', 'Ls',
     'Golf', 'Camry', 'Carnival', 'Optima', 'Yaris', 'Corolla'];
-  const randomName = Math.floor(Math.random() * names.length);
-  const randomModel = Math.floor(Math.random() * names.length);
-  const name = `${names[randomName]} ${models[randomModel]}`;
-  return name;
+  const BrandIndex = Math.floor(Math.random() * brands.length);
+  const ModelIndex = Math.floor(Math.random() * brands.length);
+  return `${brands[BrandIndex]} ${models[ModelIndex]}`;
 }
